dc6bfaf83581f2456889fa690158625c4a3c8880
imran munawar <meimran642@gmail.com>
2019-07-02 22:22:54 +0500

    dashboard fro admin

--- app/Http/Controllers/Api/V1/StatsController.php
+++ app/Http/Controllers/Api/V1/StatsController.php
@@ -5,16 +5,22 @@ namespace App\Http\Controllers\Api\V1;
 use App\Fair;
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Company;
+use App\CompanyJob;
 
 class StatsController extends Controller
 {
     public function index()
     {
-        $fairs = Fair::where('status','=','0')->count();
-        $users = User::count();
+        $fairs     = Fair::where('status','=','0')->count();
+        $companies = Company::count();
+        $compjobs  = CompanyJob::count();
+        $users     = User::count();
         $data = [
-            'fairs' => $fairs,
-            'users' => $users
+            'fairs'     => $fairs,
+            'users'     => $users,
+            'companies' => $companies,
+            'compjobs'  => $compjobs
         ];
         return response()->json([
             "code"   => 200,
