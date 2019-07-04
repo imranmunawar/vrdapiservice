31c73348a26d7ddc6eb488698b3b2f72897bacc6
imran munawar <meimran642@gmail.com>
2019-07-04 21:57:44 +0500

    Admin dashboard

--- app/Http/Controllers/Api/V1/StatsController.php
+++ app/Http/Controllers/Api/V1/StatsController.php
@@ -7,20 +7,32 @@ use App\Http\Controllers\Controller;
 use App\User;
 use App\Company;
 use App\CompanyJob;
+use App\UserLogs;
+use Carbon\Carbon;
 
 class StatsController extends Controller
 {
     public function index()
     {
-        $fairs     = Fair::where('status','=','0')->count();
+        $fairs     = Fair::count();
         $companies = Company::count();
         $compjobs  = CompanyJob::count();
         $users     = User::count();
+        $user24hrs = UserLogs::where('created_at', '>=', Carbon::now()->subDay())->count();
+        $logs      = UserLogs::select('fairs.name as fname','users.name as uname','user_logs.user_ip', 
+                                      'user_logs.location','user_logs.device', 'user_logs.browser',
+                                      'user_logs.referrer', 'user_logs.u_id', 'user_logs.created_at')
+                               ->where('user_logs.created_at', '>=', Carbon::now()->subDay())
+                               ->leftJoin('users', 'user_logs.user_id', '=', 'users.id')
+                               ->leftJoin('fairs', 'user_logs.fair_id', '=', 'fairs.id')
+                               ->get();
         $data = [
             'fairs'     => $fairs,
             'users'     => $users,
             'companies' => $companies,
-            'compjobs'  => $compjobs
+            'compjobs'  => $compjobs,
+            'logs'      => $logs,
+            'user24hrs' => $user24hrs
         ];
         return response()->json([
             "code"   => 200,
