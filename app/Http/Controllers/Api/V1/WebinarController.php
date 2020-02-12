c242c538416524f7915572e2b16b9f659350f9ef
AliSultanKhan <alisultankhan640@gmail.com>
2020-02-13 00:48:08 +0500

    create group commet chat

--- app/Http/Controllers/Api/V1/WebinarController.php
+++ app/Http/Controllers/Api/V1/WebinarController.php
@@ -43,18 +43,10 @@ class WebinarController extends Controller
         // Create a new webinar in the database...
         $webinar = CompanyWebinar::create($request->all());
         if ($webinar) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL,"'https://api.cometondemand.net/api/v2/createGroup");
-            curl_setopt($ch, CURLOPT_HTTPHEADER,array(
-              'api-key: 51374xb73fca7c64f3a49d2ffdefbb1f2e8c76'
-            ));
-            curl_setopt($ch, CURLOPT_POST, 1);  
-            curl_setopt($ch, CURLOPT_POSTFIELDS,'GUID='.$webinar->id.'&name='.$request->title.'&type=0');
-            // Receive server response ...
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $apiResponse = curl_exec($ch);
-            $apiResponse = (array)json_decode($apiResponse);
-            curl_close ($ch);
+            $response = Curl::to('https://api.cometondemand.net/api/v2/createGroup')
+            ->withHeader('api-key: 51374xb73fca7c64f3a49d2ffdefbb1f2e8c76')
+            ->withData('GUID='.$webinar->id.'&name='.$request->title.'&type=0')
+            ->post();
         }
         if (!$webinar) {
             return response()->json([ 
@@ -106,31 +98,14 @@ class WebinarController extends Controller
         $data = $request->all(); 
         $webinar = CompanyWebinar::findOrFail($id);
         if ($webinar) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL,"'https://api.cometondemand.net/api/v2/deleteGroup");
-            curl_setopt($ch, CURLOPT_HTTPHEADER,array(
-              'api-key: 51374xb73fca7c64f3a49d2ffdefbb1f2e8c76'
-            ));
-            curl_setopt($ch, CURLOPT_POST, 1);  
-            curl_setopt($ch, CURLOPT_POSTFIELDS,'GUID='.$id);
-            // Receive server response ...
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $apiResponse = curl_exec($ch);
-            $apiResponse = (array)json_decode($apiResponse);
-            curl_close ($ch);
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL,"'https://api.cometondemand.net/api/v2/createGroup");
-            curl_setopt($ch, CURLOPT_HTTPHEADER,array(
-              'api-key: 51374xb73fca7c64f3a49d2ffdefbb1f2e8c76'
-            ));
-            curl_setopt($ch, CURLOPT_POST, 1);  
-            curl_setopt($ch, CURLOPT_POSTFIELDS,'GUID='.$id.'&name='.$request->title.'&type=0');
-            // Receive server response ...
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $apiResponse = curl_exec($ch);
-            $apiResponse = (array)json_decode($apiResponse);
-            curl_close ($ch);
+            $response = Curl::to('https://api.cometondemand.net/api/v2/deleteGroup')
+                ->withHeader('api-key: 51374xb73fca7c64f3a49d2ffdefbb1f2e8c76')
+                ->withData('GUID='.$id)
+                    ->post();
+                $response = Curl::to('https://api.cometondemand.net/api/v2/createGroup')
+                ->withHeader('api-key: 51374xb73fca7c64f3a49d2ffdefbb1f2e8c76')
+                ->withData('GUID='.$id.'&name='.$request->title.'&type=0')
+                    ->post();
         }
         $webinar->fill($data)->save();
         return response()->json([
